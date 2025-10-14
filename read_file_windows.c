9d8955141bb48eaa017013dfbbeebc1aba8152ff
timothy <caveiroussight@gmail.com>
2025-10-14 23:29:14 +0600

    windows file reading + .exe

---
+++ read_file_windows.c
@@ -0,0 +1,35 @@
+#include <windows.h>
+#include <stdio.h>
+
+int main() {
+    HANDLE hFile = CreateFile(
+        "file.txt",
+        GENERIC_WRITE,
+        0,
+        NULL,
+        CREATE_ALWAYS,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL
+    );
+
+    if (hFile == INVALID_HANDLE_VALUE) {
+        printf("ошибочка открытия файла: %lu\n", GetLastError());
+        return 1;
+    }
+
+    printf("введите текст:\n");
+    char buffer[1024];
+    DWORD written;
+    
+    while (fgets(buffer, sizeof(buffer), stdin)) {
+        if (!WriteFile(hFile, buffer, (DWORD)strlen(buffer), &written, NULL)) {
+            printf("ошибка записи: %lu\n", GetLastError());
+            CloseHandle(hFile);
+            return 1;
+        }
+    }
+
+    CloseHandle(hFile);
+    printf("ТЕКСТ УСПЕШНО ЗАПИСАН!\n");
+    return 0;
+}
